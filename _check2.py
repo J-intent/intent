11c9b7ca5c0173a785d81c19ce375225fdea9df2
J-intent <783273213@qq.com>
2026-06-30 15:24:34 +0800

    chore: clean up 16 temp debug/test files from development

--- _check2.py
+++
@@ -1,7 +0,0 @@
-﻿import sys
-sys.stdout.reconfigure(encoding="utf-8")
-src = open("src/mini_intent.py", encoding="utf-8").read()
-idx = src.find("elif isinstance(expr, ListExpr)")
-# find the end of this elif block
-end = src.find("\n        elif", idx)
-print(src[idx:end])
--- _check3.py
+++
@@ -1,8 +0,0 @@
-﻿import sys
-sys.stdout.reconfigure(encoding="utf-8")
-src = open("src/mini_intent.py", encoding="utf-8").read()
-# Find execute_function body execution section  
-idx = src.index("result = IntValue(0)")
-end = src.index("\n        # 验证后置条件", idx + 100)
-ctx = src[idx-50:end+50]
-print(ctx)
